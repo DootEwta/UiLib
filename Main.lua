6a1dc5af60c42a6e1244f70269ed300fa7d9653b
DootEwta <80942639+DootEwta@users.noreply.github.com>
2021-03-19 02:09:19 -0500

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -237,8 +237,8 @@ function OwlLib.Content:newBtn(title, callback, noToggle)
         self:initBtnEffect(btn);
 
         local toggle = {
-            [true] = fromRGB(Values.EnabledColor or Color3.new(0,255,0)),
-            [false] = fromRGB(Values.DisabledColor or Color3.new(0,0,255))
+            [true] = Values.EnabledColor or Color3.new(0,255,0),
+            [false] = Values.DisabledColor or Color3.new(0,0,255)
         };
 
         tweenService:Create(btn.statusFrame, TweenInfo.new(0.25, Enum.EasingStyle.Quad, Enum.EasingDirection.Out), {BackgroundColor3 = toggle[enabled]}):Play();
